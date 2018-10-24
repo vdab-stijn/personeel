953beabd7a92208351e38c7d11f6feecbff5bd55
Stijn Verholen <Stijn.Verholen@ad.core.vdabantwerpen.intra>
2018-10-24 08:36:49 +0200

    Initial Spring Boot project

---
+++ src/main/java/be/vdab/personeel/PersoneelApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.personeel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoneelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoneelApplication.class, args);
+	}
+}
